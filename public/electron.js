c14d87e3c21fa83650909e593d8f17a3c0ba5932
thwjd12333 <thwjd12333@naver.com>
2024-09-27 11:33:39 +0900

    setting

--- public/electron.js
+++ public/electron.js
@@ -1,13 +1,13 @@
-import { ipcMain } from "electron";
+import { ipcMain } from "electron"
 
-const { app, BrowserWindow } = await import("electron");
-const path = await import("path");
-const isDev = await import("electron-is-dev");
+const { app, BrowserWindow } = await import("electron")
+const path = await import("path")
+const isDev = await import("electron-is-dev")
 
-let mainWindow;
+let mainWindow
 
 function createWindow() {
-  const __dirname = path.resolve();
+  const __dirname = path.resolve()
 
   mainWindow = new BrowserWindow({
     width: 900,
@@ -18,42 +18,42 @@ function createWindow() {
       devTools: isDev,
       preload: path.join(__dirname, "public/preload.mjs"),
     },
-  });
+  })
 
   // ***중요***
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
-  );
+  )
 
   //code:-32601 오류 발생 시작할때 console확인 가능한 창이 뜨는데 그때 뜸 실제 실행할땐 문제 없는 오류!
-  if (isDev) mainWindow.webContents.openDevTools({ mode: "detach" });
+  // if (isDev) mainWindow.webContents.openDevTools({ mode: "detach" });
 
-  mainWindow.setResizable(true);
+  mainWindow.setResizable(true)
   mainWindow.on("closed", () => {
-    mainWindow = null;
-    app.quit();
-  });
-  mainWindow.focus();
+    mainWindow = null
+    app.quit()
+  })
+  mainWindow.focus()
 }
 
-app.on("ready", createWindow);
+app.on("ready", createWindow)
 
 app.on("activate", () => {
-  if (mainWindow === null) createWindow();
-});
+  if (mainWindow === null) createWindow()
+})
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
+  if (process.platform !== "darwin") app.quit()
+})
 
 //render => main setting
 ipcMain.on("sT", (e) => {
   setTimeout(() => {
-    e.sender.send("rT", "test");
-  }, 3000);
-});
+    e.sender.send("rT", "test")
+  }, 3000)
+})
 
 // setTimeout((e) => {
 //   ipcMain.send("timer", "timer 3");
